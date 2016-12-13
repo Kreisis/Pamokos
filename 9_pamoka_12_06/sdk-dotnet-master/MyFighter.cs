eda95ad7a51badd6392b9afb279ca5d882b9836b
frida129 <julija.kondrotaite@gmail.com>
2016-12-13 18:15:35 +0200

    10 pamoka

--- 9_pamoka_12_06/sdk-dotnet-master/MyFighter.cs
+++ 9_pamoka_12_06/sdk-dotnet-master/MyFighter.cs
@@ -8,25 +8,65 @@ namespace CodeFights
 {
     class MyFighter : IFighter
     {
+        public enum Strategies
+        {
+            Counter,
+            Mimic,
+            Random,
+            Other
+        }
         Random _random;
+
+        int turn = 0;
+
         public MyFighter()
         {
             _random = new Random();
         }
+
+        Strategies strategy = Strategies.Other;
+
         public Move MakeNextMove(Move opponentsLastMove, int myLastScore, int opponentsLastScore)
         {
 
             Move thisMove = new Move();
 
+            if (turn % 2 == 0)
+            {
+                ActRandom(thisMove);
+            }   
+                 
+            else
+            {
+                MimicOppenentsLastMove(thisMove, opponentsLastMove);
+                //CounterOpponentsLastMove(thisMove, opponentsLastMove);
+            }
+
+            /*if(opponentsLastMove != null)
+            {
+                MimicOppenentsLastMove(thisMove, opponentsLastMove);
+            }
+            else
+            {
+                ActRandom(thisMove);
+            }*/
+
+            turn++;
+            return thisMove;
+        }
+
+        private void ActRandom(Move thisMove)
+        {
             List<Area> defenses = new List<Area>();
+
             for (int x = 0; x < 3; x++)
             {
                 int rnd = _random.Next(0, 11);
-                
-                if(rnd < 8)
+
+                if (rnd < 8)
                 {
                     int temp = _random.Next(0, 3);
-                    if(temp == 0)
+                    if (temp == 0)
                     {
                         thisMove.AddAttack(Area.Belly);
                     }
@@ -87,7 +127,7 @@ namespace CodeFights
                             }
                         }
                     }
-                    
+
                     else if (temp == 2)
                     {
                         if (!defenses.Contains(Area.Nose))
@@ -111,8 +151,45 @@ namespace CodeFights
                     }
                 }
             }
-            
-            return thisMove;
         }
+
+        private void MimicOppenentsLastMove(Move thisMove, Move opponentsLastMove)
+        {
+            List <Area> hisAttacks = opponentsLastMove.Attacks.ToList<Area>();
+            List <Area> hisDefenses = opponentsLastMove.Defences.ToList<Area>();
+
+            foreach (Area ar in hisAttacks)
+            {
+                thisMove.AddAttack(ar);
+            }
+            foreach (Area ar in hisDefenses)
+            {
+                thisMove.AddDefence(ar);
+            }
+
+        }
+
+        private void CounterOpponentsLastMove(Move thisMove, Move opponentsLastMove)
+        {
+            List<Area> hisAttacks = opponentsLastMove.Attacks.ToList<Area>();
+            List<Area> hisDefenses = opponentsLastMove.Defences.ToList<Area>();
+
+            foreach (Area ar in hisAttacks)
+            {
+                thisMove.AddDefence(ar);
+            }
+            foreach (Area ar in hisDefenses)
+            {
+                thisMove.AddAttack(ar);
+            }
+        }
+
+        private void PunchHard(Move thisMove, Move opponentsLastMove)
+        {
+            thisMove.AddAttack(Area.Nose);
+            thisMove.AddAttack(Area.Jaw);
+            thisMove.AddDefence(Area.Nose);
+        }
+        
     }
 }
